bf991ca81993fbd34ade051e433872bf87845760
Oscar Saharoy <osaharoy@gmail.com>
2025-08-29 07:03:14 +0100

    mobile fixes

--- site/src/home.jsx
+++ site/src/home.jsx
@@ -21,7 +21,7 @@ function NavBar() {
         <p style={{ fontSize: "1rem", paddingTop: ".4rem" }}>
             /fʲɨsˠ/ <span style={{ fontSize: "0.7rem", fontWeight: 600 }}>IRISH</span> • <strong>m</strong> <em>(genitive singular feasa)</em> • knowledge, information
         </p>
-        <nav style={{ display: "grid", gridAutoFlow: "column", gap: "3rem", justifySelf: "end" }}>
+        <nav style={{ display: "flex", gap: "0.6rem 2.4rem", justifySelf: "end", flexWrap: "wrap", justifyContent: "end" }}>
             <p><a href="/"> home </a></p>
             <p><a href="/product"> product </a></p>
             <p><a href="/about"> about </a></p>
@@ -31,7 +31,7 @@ function NavBar() {
 }
 
 function Page({ children }) {
-    return <main style={{ width: "100vw", minHeight: "100vh", display: "grid", padding: "3rem 3rem 2rem", gap: "1rem", gridTemplateRows: "1fr max-content" }}>
+    return <main style={{ width: "100vw", minHeight: "100svh", display: "grid", padding: "3rem 3rem 2rem", gap: "1rem", gridTemplateRows: "1fr max-content" }}>
         <div style={{ gridRow: 1, gridColumn: 1, display: "grid" }}>
             { children }
         </div>
@@ -60,6 +60,10 @@ export function Contact() {
 export function Product() {
     return <Page>
         <div style={{ display: "grid", alignContent: "center", justifySelf: "center", maxWidth: "70rem" }}>
+            <p> knowledgebase collation </p>
+            <p> system design and evaluation </p>
+            <p> deployment and maintenance </p>
+            <p> integrations </p>
         </div>
     </Page>;
 }
@@ -67,14 +71,14 @@ export function Product() {
 export function About() {
     return <Page>
         <div style={{ display: "grid", alignContent: "center", justifySelf: "center", maxWidth: "70rem", gap: "1rem" }}>
-            <h1 style={{ fontSize: "2.5rem", marginBottom: "1rem" }}> We are a small team building tools and models to empower researchers in the present and future. </h1>
+            <h1 style={{ marginBottom: "1rem" }}> We are a small team building tools and models to empower researchers in the present and future. </h1>
             <p> Discovering and communicating the truth is a uniquely human activity that will continue to be vital to our societies. As the volume and
                 detail demanded from researchers continues to rise, alongside misinformation and noise, new tooling is needed to answer the
                 toughest queries. </p>
             <p> We have deep experience in data mining, search, and AI systems. Having honed our techniques on our own problems, we are
                 now opening our platform for general use. </p>
             <span style={{ height: "1rem" }} />
-            <p> <strong>Oscar Saharoy</strong> | <em> Cambridge, Five AI, Meta </em></p>
+            <p style={{ marginBottom: "4rem" }}> <strong>Oscar Saharoy</strong> | <em> Cambridge, Five AI, Meta </em></p>
         </div>
     </Page>;
 }
